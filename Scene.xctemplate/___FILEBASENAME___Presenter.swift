1c9971efe3ae3bdeab5987265331d5c799b6520f
Dio Ilham <iprabawa9@gmail.com>
2019-07-05 16:47:20 +0700

    create scene

---
+++ Scene.xctemplate/___FILEBASENAME___Presenter.swift
@@ -0,0 +1,14 @@
+//___FILEHEADER___
+
+import UIKit
+
+protocol ___FILEBASENAMEASIDENTIFIER___Logic {
+   
+}
+
+class ___FILEBASENAMEASIDENTIFIER___: ___FILEBASENAMEASIDENTIFIER___Logic {
+    
+    weak var viewController: ___VARIABLE_productName:identifier___ViewController?
+    
+}
+
